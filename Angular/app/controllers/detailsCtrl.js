bbb6c4a76071f38a29f4d757b795be3b6bd62297
William Hickok <William Hickok>
2016-06-16 22:39:17 -0500

    working on adding rental dates. getting access to them in the front end, but they are not being passed into the backend

--- Angular/app/controllers/detailsCtrl.js
+++ Angular/app/controllers/detailsCtrl.js
@@ -5,16 +5,21 @@ SwapNShop.controller("detailsCtrl", [
   "$http",
   "$location",
   "EquipFactory",
+  "AuthFactory",
 
-  function ($scope, $http, $location, EquipFactory) {
+  function ($scope, $http, $location, EquipFactory, AuthFactory) {
     // Activate modal for rental
     $('.modal-trigger').leanModal();
 
     $scope.rentalDates;
   	$scope.equipment;
-
+    let rentalDatesObject = {};
     let curEquipID = EquipFactory.getEquipment();
-    console.log("curEquipID", curEquipID)
+    let curMusicicanID = AuthFactory.getUser();
+    
+    rentalDatesObject.IdEquipment = curEquipID;
+    rentalDatesObject.IdMusician = curMusicicanID;
+    console.log("curEquipID", rentalDatesObject)
 
     // Get access to the specific item the user clicked on
 		$http
@@ -25,31 +30,38 @@ SwapNShop.controller("detailsCtrl", [
 			});
 		
     // post rental request to the database
-    $scope.requestRent = function (files) {
+    $scope.requestRent = function () {
       console.log("rent", $scope.rentalDates)
 
+      // Build out the object to send to the database
+      rentalDatesObject.checkOutDates = $scope.rentalDates.check_OUT_date;
+      rentalDatesObject.checkInDates = $scope.rentalDates.check_IN_date;
+
+      console.log("rentalDatesObject", rentalDatesObject)
+
+
+      // post to the database
+      $http({
+        url:'http://localhost:49881/api/RentalDates',
+        method: 'POST',
+        data: JSON.stringify($scope.rentalDatesObject)
+      })
+      .success(function newEquipment (){
+        console.log('201 Created', rentalDatesObject)
+        // $location.path("/homepage");
+      })
+
+      // Post that the equipment has been requested to be rented
       $http({
         url:`http://localhost:49881/api/Equipment/${curEquipID}`,
         method: 'PUT',
         // data: JSON.stringify($scope.equipment)
       })
       .success(function newEquipment (){
-        console.log('201 Created', newEquipment)
-          $location.path("/homepage");
+        console.log('201 updated')
       })	
     }
 	}
 ]);
 
 
-
-
-      // $http({
-      //   url:'http://localhost:49881/api/Equipment',
-      //   method: 'POST',
-      //   data: JSON.stringify($scope.equipment)
-      // })
-      // .success(function newEquipment (){
-      //   console.log('201 Created', newEquipment)
-      //     $location.path("/homepage");
-      // })  
